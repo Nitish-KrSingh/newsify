a29416b0356137ec020d2ae49b3597193a556251
Nitish <nitishbcps@gmail.com>
2023-04-30 01:54:36 +0530

    project done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const axios = require('axios')
 const path = require('path')
-
 const app = express()
 const port = 3000
 
@@ -10,11 +9,12 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api', async(req, res) => {
-    console.log(req._parsedUrl.query)
+    
     let url = "https://newsapi.org/v2/everything?"+req._parsedUrl.query
     let r = await axios(url)
     let a = r.data
    res.json(a)
+   console.log(req._parsedUrl.query)
   })
 
 
